bf063f973948ed4f871578440dc29d29084795f8
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-04-05 22:23:55 +0900

    Initial Commit

---
+++ GNEcommerce/GNEcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNEcommerceApp.swift
+//  GNEcommerce
+//
+//  Created by joe on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct GNEcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
